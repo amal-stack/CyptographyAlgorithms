136237157055896acfc8cac2c175c8389e5ced70
amalkrishna <amalkrishna263@outlook.com>
2022-12-29 22:19:08 +0530

    Add monoalphabetic substitution cipher

---
+++ CyptographyAlgorithms/Substitution/MonoalphabeticSubstitutionCipher.cs
@@ -0,0 +1,21 @@
+﻿namespace CyptographyAlgorithms.Substitution;
+
+public class MonoalphabeticSubstitutionCipher : ICipher
+{
+    public MonoalphabeticSubstitutionCipher(PermutationTable table) => Table = table;
+
+    public PermutationTable Table { get; }
+
+    public string Encipher(string plaintext) => string.Concat(
+        from c in plaintext
+        select Table[c]
+    );
+
+    public string Decipher(string ciphertext) => string.Concat(
+        from c in ciphertext
+        select Table[c, inverse: true]
+    );
+
+}
+
+
