e579909f25b54f334d0fcd1ace79b80762f1c3af
amalkrishna <amalkrishna263@outlook.com>
2022-12-29 22:18:47 +0530

    Add shift cipher

---
+++ CyptographyAlgorithms/Shift/BruteForceShiftCipherBreaker.cs
@@ -0,0 +1,20 @@
+﻿namespace CyptographyAlgorithms.Shift;
+
+public class BruteForceShiftCipherBreaker : IBruteForceCipherBreaker<ShiftCipher, BruteForceResult>
+{
+    public IEnumerable<BruteForceResult> Generate(string ciphertext)
+    {
+        foreach (var shiftValue in Enumerable.Range(0, 26))
+        {
+            yield return new BruteForceResult
+            {
+                Plaintext = new ShiftCipher(shift: shiftValue).Decipher(ciphertext),
+                Shift = shiftValue
+            };
+        }
+    }
+}
+
+public record struct BruteForceResult(
+        string Plaintext,
+        int Shift);
---
+++ CyptographyAlgorithms/Shift/ShiftCipher.cs
@@ -0,0 +1,26 @@
+﻿using CyptographyAlgorithms.Extensions;
+
+namespace CyptographyAlgorithms.Shift;
+
+public class ShiftCipher : ICipher
+{
+    public ShiftCipher(int shift) => Shift = shift;
+
+    public int Shift { get; }
+
+    public string Encipher(string plaintext) => ShiftChars(plaintext, Shift);
+
+    public string Decipher(string ciphertext) => ShiftChars(ciphertext, -Shift);
+
+    private static string ShiftChars(IEnumerable<char> chars, int shift) => string.Concat(
+        from c in chars
+        select ShiftChar(c, shift)
+    );
+
+    private static char ShiftChar(char c, int shift) => c switch
+    {
+        _ when char.IsUpper(c) => Convert.ToChar(ModularArithmetic.Mod(c - 'A' + shift, 26) + 'A'),
+        _ when char.IsLower(c) => Convert.ToChar(ModularArithmetic.Mod(c - 'a' + shift, 26) + 'a'),
+        _ => c
+    };
+}
