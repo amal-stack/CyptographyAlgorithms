cd6dfe0e2067a58fc7d2523ebafb4f7814623fb9
amalkrishna <amalkrishna263@outlook.com>
2023-01-28 17:47:45 +0530

    Move formatters to separate type Formatters

---
+++ CyptographyAlgorithms/Extensions/Formatters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace CyptographyAlgorithms.Extensions;
+
+public static class Formatters
+{
+    public static string ToBitString(this byte[] bytes, char delimiter = ' ') => string.Join(
+            "",
+            bytes
+                .SelectMany(@byte => Convert
+                    .ToString(@byte, 2)
+                    .PadLeft(8, '0')
+                    .Select((bit, idx) => (idx + 1) % 4 == 0 ? $"{bit} " : bit.ToString()))
+        );
+
+    public static string ToBitString(this BitArray bitArray, char delimiter = ' ') => string.Concat(
+        bitArray
+            .OfType<bool>()
+            //.Reverse()
+            .Select((i, idx) => (i ? 1 : 0) + ((idx + 1) % 8 == 0 ? delimiter.ToString() : ""))
+    );
+
+}
