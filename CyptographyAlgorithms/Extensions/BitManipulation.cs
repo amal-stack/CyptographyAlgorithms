4963f048512619cb8476860ed16fb7fc18457144
amalkrishna <amalkrishna263@outlook.com>
2023-01-28 17:47:00 +0530

    Add GetBitAndByteIndexes method

--- CyptographyAlgorithms/Extensions/BitManipulation.cs
+++ CyptographyAlgorithms/Extensions/BitManipulation.cs
@@ -4,12 +4,31 @@ public static class BitManipulation
 {
     public static int GetMask(int bitCount)
     {
-        if (bitCount > ByteExtensions.BitsPerByte)
+        if (bitCount > BitConstants.BitsPerByte)
         {
             throw new ArgumentOutOfRangeException(
                 nameof(bitCount),
-                $"The maximum number of bits per byte is {ByteExtensions.BitsPerByte}");
+                $"The maximum number of bits per byte is {BitConstants.BitsPerByte}");
         }
         return (int)Math.Pow(2, bitCount) - 1;
+    }
+
+    public static (int ByteIndex, int BitIndex) GetBitAndByteIndexes(
+        int bitIndex, 
+        int length, 
+        Endianness endianness, 
+        BitNumbering bitNumbering)
+    {
+        var (outputbyteIndex,outputBitIndex) = Math.DivRem(bitIndex, BitConstants.BitsPerByte);
+
+        if (endianness is Endianness.LittleEndian)
+        {
+            outputbyteIndex = length - 1 - outputbyteIndex;
+        }
+        if (bitNumbering is BitNumbering.Msb0)
+        {
+            outputBitIndex = BitConstants.BitsPerByte - 1 - outputBitIndex;
+        }
+        return (outputbyteIndex, outputBitIndex);
     }
 }
