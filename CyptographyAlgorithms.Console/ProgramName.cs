5fd9bf79c81a04d45c47a4ed6a70a5642cf294c1
amalkrishna <amalkrishna263@outlook.com>
2023-01-29 16:39:55 +0530

    Add ProgramName enum

---
+++ CyptographyAlgorithms.Console/ProgramName.cs
@@ -0,0 +1,24 @@
+﻿namespace CyptographyAlgorithms.Console;
+
+public enum ProgramName
+{
+    None,
+    ShiftCipher,
+    MonoalphabeticSubstitutionCipher,
+    Spn,
+    Lfsr,
+    Des,
+    Aes,
+    AesKeySchedule,
+    KnapsackCryptosystem,
+    RsaSignatureScheme
+}
+
+
+class NullRunner
+{
+    public static void Run()
+    {
+
+    }
+}
