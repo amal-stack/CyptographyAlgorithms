a81c9031ce61f2bb6cce333e0facf938e9b17e99
amalkrishna <amalkrishna263@outlook.com>
2023-01-29 16:41:30 +0530

    Add main program

--- CyptographyAlgorithms.Console/Program.cs
+++ CyptographyAlgorithms.Console/Program.cs
@@ -1,13 +1,41 @@
-﻿namespace CyptographyAlgorithms.Console;
+﻿using CyptographyAlgorithms.Console;
+
+ProgramName programName = ProgramName.KnapsackCryptosystem;
 
 
-internal class Program
+Dictionary<ProgramName, Type> programTypes = new()
 {
-    public static void Main()
-    {
-        //RsaSignatureSchemeRunner.Run();
-        //KnapsackRunner.Run();
-        //SpnRunner.Run();
-        AesRunner.Run();
-    }
+    { ProgramName.None, typeof(NullRunner) },
+    { ProgramName.ShiftCipher, typeof(ShiftCipherRunner) },
+    { ProgramName.MonoalphabeticSubstitutionCipher, typeof(MonoalphabeticSubtitutionCipherRunner) },
+    { ProgramName.Spn, typeof(SpnRunner) },
+    { ProgramName.Lfsr, typeof(LfsrRunner) },
+    { ProgramName.Aes, typeof(AesRunner) },
+    { ProgramName.AesKeySchedule, typeof(AesKeyScheduleRunner) },
+    { ProgramName.KnapsackCryptosystem, typeof(KnapsackRunner) },
+    { ProgramName.RsaSignatureScheme, typeof(RsaSignatureSchemeRunner) },
+};
+
+
+
+
+if (programTypes.TryGetValue(programName, out Type? type))
+{
+    type.GetMethod("Run")?.Invoke(null, Array.Empty<object>());
+}
+
+
+
+enum ProgramName
+{
+    None = 0,
+    ShiftCipher = 1,
+    MonoalphabeticSubstitutionCipher = 2,
+    Spn = 3,
+    Lfsr = 4,
+    Des = 5,
+    Aes = 6,
+    AesKeySchedule = 7,
+    KnapsackCryptosystem = 8,
+    RsaSignatureScheme = 9
 }
